4c89ea9c72228c8f49ba7a2b0eaf08a8956c250f
oishikatta90 <xxxwon90@gmail.com>
2021-12-15 17:18:39 +0900

    게시물 컨트롤러 추가!

--- src/main/java/com/sbs/untact/controller/UsrHomeController.java
+++ src/main/java/com/sbs/untact/controller/UsrHomeController.java
@@ -1,17 +1,34 @@
 package com.sbs.untact.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.sbs.untact.dto.Article;
+
 @Controller
 public class UsrHomeController {
-	
-	@RequestMapping("/usr/home/main")
+	private List<Article> articles;
+
+	public UsrHomeController() {
+		articles = new ArrayList<>();
+		articles.add(new Article(1, "2020-12-12 12:12:12", "제목1", "내용1"));
+		articles.add(new Article(2, "2020-12-12 12:12:12", "제목2", "내용2"));
+
+	}
+
+	@RequestMapping("/usr/article/detail")
 	@ResponseBody
-	public String showMain() {
-		System.out.println("Hello");
-		return "123123214";
+	public Article showDetail(int id) {
+		return articles.get(id - 1);
+	}
+
+	@RequestMapping("/usr/article/list")
+	@ResponseBody
+	public List<Article> showList() {
+		return articles;
 	}
 }
