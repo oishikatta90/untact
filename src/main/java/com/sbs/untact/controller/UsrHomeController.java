313267438d526cbd722530da38de7650530d1c21
oishikatta90 <xxxwon90@gmail.com>
2021-12-15 18:12:54 +0900

    게시물 삭제

--- src/main/java/com/sbs/untact/controller/UsrHomeController.java
+++ src/main/java/com/sbs/untact/controller/UsrHomeController.java
@@ -49,4 +49,31 @@ public class UsrHomeController {
 		rs.put("id", articlesLastId);
 		return rs;
 	}
+	
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public Map<String, Object> doDelete(int id) {
+		boolean deleteArticleRs = deleteArticle(id);
+		Map<String, Object> rs = new HashMap<>();
+		
+		if (deleteArticleRs) {
+			rs.put("resultCode", "S-1");
+			rs.put("msg", "성공하셨습니다.");
+		} else {
+			rs.put("resultCode", "F-1");
+			rs.put("msg", "해당 게시물은 존재하지 않습니다.");
+		}
+		rs.put("id", id);
+		return rs;
+	}
+
+	private boolean deleteArticle(int id) {
+		for (Article article : articles) {
+			if (article.getId() == id ) {
+				articles.remove(article);
+				return true;
+			}
+		}
+		return false;
+	}
 }
