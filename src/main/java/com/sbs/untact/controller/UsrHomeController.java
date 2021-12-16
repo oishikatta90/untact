9dfc25a491685f4c9b75dfa1998bbd7805cd11b7
oishikatta90 <xxxwon90@gmail.com>
2021-12-16 12:58:11 +0900

    출력 양식을 UtilMapOf에서 ResultData 클래스를 만들어 진회시킴

--- src/main/java/com/sbs/untact/controller/UsrHomeController.java
+++ src/main/java/com/sbs/untact/controller/UsrHomeController.java
@@ -1,8 +1,6 @@
 package com.sbs.untact.controller;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sbs.untact.dto.Article;
+import com.sbs.untact.dto.ResultData;
 import com.sbs.untact.util.Util;
 
 @Controller
@@ -45,25 +44,25 @@ public class UsrHomeController {
 
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
-	public Map<String, Object> doAdd(String title, String body) {
+	public ResultData doAdd(String title, String body) {
 		String regDate = Util.getNowDateStr();
 		String updateDate = regDate;
 
 		articles.add(new Article(++articlesLastId, regDate, updateDate, title, body));
 
-		return Util.mapOf("resultCode", "S-1", "msg", "성공하셨습니다.", "id", articlesLastId);
+		return new ResultData("S-1", "성공하였습니다.", "id", articlesLastId);
 	}
 
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
-	public Map<String, Object> doDelete(int id) {
+	public ResultData doDelete(int id) {
 		boolean deleteArticleRs = deleteArticle(id);
 		Map<String, Object> rs = new HashMap<>();
 
 		if (deleteArticleRs == false) {
-			return Util.mapOf("resultCode", "F-1", "msg", "해당 게시물은 존재하지 않습니다.");
+			return new ResultData("F-1", "해당 게시물은 존재하지 않습니다.");
 		}
-		return Util.mapOf("resultCode", "S-1", "msg","성공하셨습니다.", "id",id);
+		return new ResultData("F-1", "성공하셨습니다.","id",id);
 	}
 
 	private boolean deleteArticle(int id) {
@@ -78,7 +77,7 @@ public class UsrHomeController {
 
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
-	public Map<String, Object> doModify(int id, String title, String body) {
+	public ResultData doModify(int id, String title, String body) {
 		Article selArticle = null;
 
 		for (Article article : articles) {
@@ -91,14 +90,14 @@ public class UsrHomeController {
 		Map<String, Object> rs = new HashMap<>();
 
 		if (selArticle == null) {
-			return Util.mapOf("resultCode", "F-1", "msg", String.format("%d번 게시물은 존재하지 않습니다.", id));
+			return new ResultData("F-1", String.format("%d번 게시물은 존재하지 않습니다.", id));
 		}
 
 		selArticle.setUpdateDate(Util.getNowDateStr());
 		selArticle.setTitle(title);
 		selArticle.setBody(body);
 
-		return Util.mapOf("resultCode", "S-1", "msg", String.format("%d번 게시물이 수정되었습니다.", id),"id", id);
+		return new ResultData("S-1", String.format("%d번 게시물이 수정되었습니다.", id),"id", id);
 	}
 
 }
